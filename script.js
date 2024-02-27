ed9a8a52306b478deba09e71b3c704759f30c689
Dreamati <156924641+Dreamati@users.noreply.github.com>
2024-02-27 13:40:33 +0530

    final Product

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.querySelector('input');
 const body = document.querySelector('body');
 const bigBox = document.querySelector('.bigBox');
+let isRainbow = false;
 
 input.addEventListener('click', boxCreate);
 const clear = document.querySelector('.clear')
@@ -27,12 +28,30 @@ function boxCreate() {
 
         for(let j = 0; j< boxNumber; j++)
         {
+            let colors = ['red', 'blue', 'orange', 'yellow', 'green', 'indigo', 'violet'];
             let grandchildBox = document.createElement('div');
             grandchildBox.classList.add('grandchildBox');
             childBox.appendChild(grandchildBox);
-
+            if(isRainbow)
+            {
+                grandchildBox.addEventListener('mousemove', () => grandchildBox.style.backgroundColor = colors[Math.floor(Math.random() * 7)]);
+            }
+            else{
             grandchildBox.addEventListener('mousemove', () => grandchildBox.classList.add('clicked'))
+            }
         }
     }
 
 }
+
+document.querySelector('.rainbow').addEventListener('click', function(){
+
+    if (isRainbow)
+    {
+        isRainbow = false;
+    }
+    else {
+        isRainbow = true;
+    }
+    boxCreate();
+}) 
